e3e6d01cac160594774904e286f97a561f87c436
Stanley Smith <stancsmith@gmail.com>
2016-03-23 15:13:59 -0400

    working php response

--- apiRequest.php
+++ apiRequest.php
@@ -6,9 +6,11 @@
 		$zipCode = $_GET['zipCode'];
 		$distance = $_GET['distance'];
 
-		$fullUrl = $url . '&zipCode=' . $zipCode . '&distance=' . $distance;
+		$fullUrl = $mentoringUrl . '&zipCode=' . $zipCode . '&distance=' . $distance;
 	} else {
-		
+		$vol_loc = $_GET['vol_loc'];
+		$vol_dist = $_GET['vol_dist'];
+		$fullUrl = $allForGoodUrl . '&vol_loc=' . $vol_loc . '&vol_dist=' . $vol_dist;
 	}
 	
 	$JSON = file_get_contents($fullUrl);
